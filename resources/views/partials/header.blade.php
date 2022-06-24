f0998102ca9e5d03528156a4969aa12fb8ceefa8
davide1217 <davidepizzola17@gmail.com>
2022-06-24 10:29:09 +0200

    più-completo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,7 @@
 
         <div class="d-flex align-items-center"><img src="img/dc-logo.png" alt=""></div>
         <ul>
+            <li><a href="/">home</a></li>
         @foreach ($headerMenu as $link)
             <li>
                 <a href="{{$link}}">{{ $link }}</a>
